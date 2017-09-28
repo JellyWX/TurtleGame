8edb143dd9b88ffe5b5339dceb2898e4acd3402c
JellyWX <judewrs@gmail.com>
2017-09-28 16:52:46 +0100

    initial

---
+++ TurtleTools.py
@@ -0,0 +1,32 @@
+import turtle
+
+def ReadVertex(fileN):
+  with open(fileN,'r') as f:
+    instr = []
+    for line in f:
+      line = line.replace(' ','').replace('\n','').split('>')
+      point = []
+      for i in range(len(line)):
+        point.append(
+          tuple(map(float,line[i].split(',')))
+        )
+
+      instr.append(point)
+
+  return instr
+
+def PaintVertex(vertexes,scale=1,pos=(0,0)):
+  T = turtle.Turtle()
+  T.ht()
+  T.speed(0)
+
+  for instruction in vertexes:
+    print('instruction ', instruction)
+
+    T.penup()
+    T.setx(instruction[0][0] * scale + pos[0])
+    T.sety(instruction[0][1] * scale + pos[1])
+    T.pendown()
+
+    for coord in instruction:
+      T.goto(coord[0] * scale + pos[0],coord[1] * scale + pos[1])
---
+++ main.py
@@ -0,0 +1,7 @@
+from TurtleTools import *
+
+square = ReadVertex('obj.tur')
+
+PaintVertex(square,scale=50)
+PaintVertex(square,scale=100)
+PaintVertex(square,scale=50,pos=(10,20))
